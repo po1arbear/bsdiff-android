97b8fb8c220a5aa3996719ea426ca4c7dac9a287
熊壮(003357) <xiongzhuang@fcbox.com>
2021-01-06 18:33:09 +0800

    feat:add 示例代码

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,25 +3,30 @@ package com.example.myapplication
 import android.Manifest
 import android.app.Activity
 import android.content.Context
-import android.content.Intent
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.fcbox.diffupdate.PatchUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
-
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_EXTERNAL_STORAGE = 1
     private val PERMISSIONS_STORAGE = arrayOf(
         Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
+    private val executor: Executor = Executors.newSingleThreadExecutor()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,17 +43,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun applyAPK() {
 
-
+        // 获取任意app的apk包
+        val hiveboxInfo = packageManager.getApplicationInfo("com.fcbox.hiveconsumer",0)
+        Log.d("hiveboxInfo:",hiveboxInfo.sourceDir)
 //        var oldpath = (Environment.getExternalStorageDirectory().absolutePath + File.separator
 //                + "bs_old.apk")
 
         // 获取旧apk路径   applicationInfo.sourceDir
-        var oldpath = applicationInfo.sourceDir;
+        val oldpath = applicationInfo.sourceDir;
 
-        var newpath = (Environment.getExternalStorageDirectory().absolutePath + File.separator
+        val newpath = (Environment.getExternalStorageDirectory().absolutePath + File.separator
                 + "composed_apk.apk")
 
-        var patchpath = (Environment.getExternalStorageDirectory().absolutePath + File.separator
+        val patchpath = (Environment.getExternalStorageDirectory().absolutePath + File.separator
                 + "bs_patch")
 
 
@@ -60,11 +67,49 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("bstest", "patchpath---->$patchpath  oldpath---->$oldpath   newpath---->$newpath");
 
+        val start = System.currentTimeMillis()
+        val builder = AlertDialog.Builder(this)
+        builder.setIcon(R.drawable.ic_launcher_background)
+        val dialog = builder.show()
 
 
-        PatchUtils.patch(oldpath, newpath, patchpath);
-
+        executor.execute {
+            val ret = PatchUtils.patch(oldpath, newpath, patchpath)
+            Log.d("patchResult", "ret:$ret  cost time---->" + (System.currentTimeMillis() - start))
+            dialog.dismiss()
+        }
     }
+
+
+
+
+//        GlobalScope.launch {
+//            val oldFile = File(oldpath)
+//            val patchFile = File(patchpath)
+//            if(!oldFile.exists()){
+//                Toast.makeText(this@MainActivity, "旧包不存在", Toast.LENGTH_LONG).show()
+//                return@launch
+//            }
+//
+//            if(!patchFile.exists()){
+//                Toast.makeText(this@MainActivity, "补丁包不存在", Toast.LENGTH_LONG).show()
+//                return@launch
+//            }
+//            val ret = patch(oldpath, newpath, patchpath)
+//            if (ret == 0) {
+//                Toast.makeText(this@MainActivity, "aaa", Toast.LENGTH_LONG).show()
+//            }
+//
+//        }
+
+//        PatchUtils.patch(oldpath,newpath,patchpath)
+
+
+
+    private suspend fun patch(old: String, new: String, patch: String): Int =
+        withContext(Dispatchers.IO) {
+            PatchUtils.patch(old, new, patch)
+        }
 
 
     private fun stream2file(path: String, fileName: String, context: Context): File {
